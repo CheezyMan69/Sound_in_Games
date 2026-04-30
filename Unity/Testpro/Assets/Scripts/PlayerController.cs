05801d970895e05dfa6c101becbd4328bee8c5e4
CheezyMan69 <karimaref2005@gmail.com>
2026-04-30 10:44:44 +0200

    added: score placeholder and pick ups

--- Unity/Testpro/Assets/Scripts/PlayerController.cs
+++ Unity/Testpro/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,23 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    public float speed = 0;
+    private int score;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
     }
 
 
     void OnMove(InputValue movementValue)
     {
-
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     // Update is called once per frame
@@ -28,6 +33,17 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+        }
         
     }
 }
